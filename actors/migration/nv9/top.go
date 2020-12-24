a7367a41a8500275072639768e422a84e8ff4c95
anorth <445306+anorth@users.noreply.github.com>
2020-12-24 14:20:04 +1100

    Add logging and non-zero queue capacity to parallel migrations.

--- actors/migration/nv9/top.go
+++ actors/migration/nv9/top.go
@@ -4,9 +4,12 @@ import (
 	"context"
 	"fmt"
 	"sync"
+	"sync/atomic"
+	"time"
 
 	address "github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/rt"
 	cid "github.com/ipfs/go-cid"
 	cbor "github.com/ipfs/go-ipld-cbor"
 	"golang.org/x/sync/errgroup"
@@ -22,7 +25,18 @@ import (
 
 // Config parameterizes a state tree migration
 type Config struct {
-	MaxWorkers int
+	// Number of migration worker goroutines to run.
+	// More workers enables higher CPU utilization doing migration computations (including state encoding)
+	MaxWorkers uint
+	// Capacity of the queue of jobs available to workers (zero for unbuffered).
+	// A queue length of hundreds to thousands improves throughput at the cost of memory.
+	JobQueueSize uint
+	// Capacity of the queue receiving migration results from workers, for persisting (zero for unbuffered).
+	// A queue length of tens to hundreds improves throughput at the cost of memory.
+	ResultQueueSize uint
+	// Time between progress logs to emit.
+	// Zero (the default) results in no progress logs.
+	ProgressLogPeriod time.Duration
 }
 
 type StateMigrationInput struct {
@@ -55,8 +69,14 @@ func (n nilMigrator) MigrateState(_ context.Context, _ cbor.IpldStore, in StateM
 	}, nil
 }
 
+type Logger interface {
+	// This is the same logging interface provided by the Runtime.
+	Log(level rt.LogLevel, msg string, args ...interface{})
+}
+
 // Migrates the filecoin state tree starting from the global state tree and upgrading all actor state.
-func MigrateStateTree(ctx context.Context, store cbor.IpldStore, actorsRootIn cid.Cid, priorEpoch abi.ChainEpoch, cfg Config) (cid.Cid, error) {
+// The store must support concurrent writes (even if the configured worker count is 1).
+func MigrateStateTree(ctx context.Context, store cbor.IpldStore, actorsRootIn cid.Cid, priorEpoch abi.ChainEpoch, cfg Config, log Logger) (cid.Cid, error) {
 	if cfg.MaxWorkers <= 0 {
 		return cid.Undef, xerrors.Errorf("invalid migration config with %d workers", cfg.MaxWorkers)
 	}
@@ -82,6 +102,7 @@ func MigrateStateTree(ctx context.Context, store cbor.IpldStore, actorsRootIn ci
 	if len(migrations)+len(deferredCodeIDs) != 11 {
 		panic(fmt.Sprintf("incomplete migration specification with %d code CIDs", len(migrations)))
 	}
+	startTime := time.Now()
 
 	// Load input and output state trees
 	adtStore := adt3.WrapStore(ctx, store)
@@ -96,13 +117,18 @@ func MigrateStateTree(ctx context.Context, store cbor.IpldStore, actorsRootIn ci
 
 	// Setup synchronization
 	grp, ctx := errgroup.WithContext(ctx)
-	inputCh := make(chan *migrationInput)
-	resultCh := make(chan *migrationResult)
+	// Input and output queues for workers.
+	inputCh := make(chan *migrationInput, cfg.JobQueueSize)
+	resultCh := make(chan *migrationResult, cfg.ResultQueueSize)
+	// Atomically-modified counters for logging progress
+	var jobCount uint32
+	var doneCount uint32
 
 	// Iterate all actors in old state root to generate migration inputs for each non-deferred actor.
 	grp.Go(func() error {
 		defer close(inputCh)
-		return actorsIn.ForEach(func(addr address.Address, actorIn *states2.Actor) error {
+		log.Log(rt.INFO, "Creating migration jobs for tree %s", actorsRootIn)
+		if err = actorsIn.ForEach(func(addr address.Address, actorIn *states2.Actor) error {
 			if _, ok := deferredCodeIDs[actorIn.Code]; ok {
 				return nil // Deferred for explicit migration later.
 			}
@@ -116,14 +142,20 @@ func MigrateStateTree(ctx context.Context, store cbor.IpldStore, actorsRootIn ci
 			case <-ctx.Done():
 				return ctx.Err()
 			}
+			atomic.AddUint32(&jobCount, 1)
 			return nil
-		})
+		}); err != nil {
+			return err
+		}
+		log.Log(rt.INFO, "Done creating %d migration jobs for tree %s after %v", jobCount, actorsRootIn, time.Since(startTime))
+		return nil
 	})
 
 	// Worker threads run migrations on inputs.
 	var workerWg sync.WaitGroup
-	for i := 0; i < cfg.MaxWorkers; i++ {
+	for i := uint(0); i < cfg.MaxWorkers; i++ {
 		workerWg.Add(1)
+		workerId := i
 		grp.Go(func() error {
 			defer workerWg.Done()
 			for input := range inputCh {
@@ -136,25 +168,59 @@ func MigrateStateTree(ctx context.Context, store cbor.IpldStore, actorsRootIn ci
 				case <-ctx.Done():
 					return ctx.Err()
 				}
+				atomic.AddUint32(&doneCount, 1)
 			}
+			log.Log(rt.INFO, "Worker %d done", workerId)
 			return nil
 		})
 	}
+	log.Log(rt.INFO, "Started %d workers", cfg.MaxWorkers)
 
-	// Close output channel when workers are done.
+	// Monitor the job queue. This non-critical goroutine is outside the errgroup and exits when
+	// workersFinished is closed, or the context done.
+	workersFinished := make(chan struct{}) // Closed when waitgroup is emptied.
+	if cfg.ProgressLogPeriod > 0 {
+		go func() {
+			defer log.Log(rt.DEBUG, "Job queue monitor done")
+			for {
+				select {
+				case <-time.After(cfg.ProgressLogPeriod):
+					jobsNow := jobCount // Snapshot values to avoid incorrect-looking arithmetic if they change.
+					doneNow := doneCount
+					pendingNow := jobsNow - doneNow
+					elapsed := time.Since(startTime)
+					rate := float64(doneNow) / elapsed.Seconds()
+					log.Log(rt.INFO, "%d jobs created, %d done, %d pending after %v (%.0f/s)",
+						jobsNow, doneNow, pendingNow, elapsed, rate)
+				case <-workersFinished:
+					return
+				case <-ctx.Done():
+					return
+				}
+			}
+		}()
+	}
+
+	// Close result channel when workers are done sending to it.
 	grp.Go(func() error {
 		workerWg.Wait()
 		close(resultCh)
+		close(workersFinished)
+		log.Log(rt.INFO, "All workers done after %v", time.Since(startTime))
 		return nil
 	})
 
 	// Insert migrated records in output state tree and accumulators.
 	grp.Go(func() error {
+		log.Log(rt.INFO, "Result writer started")
+		resultCount := 0
 		for result := range resultCh {
 			if err := actorsOut.SetActor(result.Address, &result.Actor); err != nil {
 				return err
 			}
+			resultCount++
 		}
+		log.Log(rt.INFO, "Result writer wrote %d results to state tree after %v", resultCount, time.Since(startTime))
 		return nil
 	})
 
@@ -165,6 +231,9 @@ func MigrateStateTree(ctx context.Context, store cbor.IpldStore, actorsRootIn ci
 	// Perform any deferred migrations explicitly here.
 	// Deferred migrations might depend on values accumulated through migration of other actors.
 
+	elapsed := time.Since(startTime)
+	rate := float64(doneCount) / elapsed.Seconds()
+	log.Log(rt.INFO, "All %d done after %v (%.0f/s). Flushing state tree root.", doneCount, elapsed, rate)
 	return actorsOut.Flush()
 }
 
